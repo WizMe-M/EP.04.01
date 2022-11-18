ba2e4e659027cc64b342a8f65f4ce9f570dded41
Timkin Maxim <enderman.2016@mail.ru>
2022-11-18 10:18:22 +0300

    calc view

--- Calculator/src/main/java/com/timkin/calculator/CalculatorApplication.java
+++ Calculator/src/main/java/com/timkin/calculator/CalculatorApplication.java
@@ -2,12 +2,20 @@ package com.timkin.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class CalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String openCalculator() {
+        return "calc";
     }
 
 }
