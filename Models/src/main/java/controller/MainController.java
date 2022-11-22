615e0d5eddafc9b5a9cacde67e345a2b731aba9f
Timkin Maxim <enderman.2016@mail.ru>
2022-11-22 18:44:51 +0300

    home page

--- Models/src/main/java/controller/MainController.java
+++ Models/src/main/java/controller/MainController.java
@@ -1,7 +1,12 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping
+    public String index() {
+        return "main/index";
+    }
 }
