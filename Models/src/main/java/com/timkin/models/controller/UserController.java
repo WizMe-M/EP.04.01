43835d2909d57e16d1e314cf88760b0dcb62a51a
WizMe-M <timkin.moxim@mail.ru>
2022-11-22 21:53:28 +0300

    attach all users to view

--- Models/src/main/java/com/timkin/models/controller/UserController.java
+++ Models/src/main/java/com/timkin/models/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.timkin.models.controller;
 
+import com.timkin.models.entity.User;
 import com.timkin.models.repo.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
-    private UserRepository repository;
+    private final UserRepository repository;
 
     public UserController(UserRepository repository) {
         this.repository = repository;
@@ -21,7 +25,9 @@ public class UserController {
     }
 
     @GetMapping("/all")
-    public String openAllUsersTable(){
+    public String openAllUsersTable(Model model) {
+        List<User> all = repository.findAll();
+        model.addAttribute("users", all);
         return "users/all_users";
     }
 }
