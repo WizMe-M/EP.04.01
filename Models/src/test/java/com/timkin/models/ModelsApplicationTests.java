760a1ff1a02734ac27f17df071c7c7926123f9d2
Timkin Maxim <enderman.2016@mail.ru>
2022-11-22 18:41:07 +0300

    initialized module

---
+++ Models/src/test/java/com/timkin/models/ModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timkin.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
