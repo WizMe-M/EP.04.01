4c517ff3ebc50c1144339797524267002b527f02
Timkin Maxim <enderman.2016@mail.ru>
2022-11-22 19:07:38 +0300

    moved controller to correct package

--- Models/src/main/java/com/timkin/models/controller/MainController.java
+++ Models/src/main/java/com/timkin/models/controller/MainController.java
@@ -1,12 +1,18 @@
-package controller;
+package com.timkin.models.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
     @GetMapping
     public String index() {
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String openHomePage() {
         return "main/index";
     }
 }
