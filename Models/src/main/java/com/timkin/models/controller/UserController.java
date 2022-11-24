436a5d026e8a0a8b68775e1a5a85288adc2b6498
Timkin Maxim <enderman.2016@mail.ru>
2022-11-24 14:23:31 +0300

    create and edit users with object

--- Models/src/main/java/com/timkin/models/controller/UserController.java
+++ Models/src/main/java/com/timkin/models/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
@@ -36,19 +37,48 @@ public class UserController {
     }
 
     @PostMapping("/add")
-    public String createUser(
-            User user) {
+    public String createUser(User user) {
         repository.save(user);
         return "redirect:/users/all";
     }
 
-    @GetMapping("/profile/{profile-name}")
+    @GetMapping("/{profile-name}")
     public String openProfile(
             @PathVariable(name = "profile-name") String login,
-            Model model
+            Model model,
+            User profile
     ) {
-        User user = repository.findByLogin(login).orElseThrow();
-        model.addAttribute("profile", user);
+        Optional<User> found = repository.findByLogin(login);
+        if (found.isEmpty()) {
+            return "redirect:/users/all";
+        }
+        profile = found.get();
+        model.addAttribute("profile", profile);
         return "users/profile";
+    }
+
+    @GetMapping("/{profile-name}/edit")
+    public String openEditProfile(
+            @PathVariable(name = "profile-name") String login,
+            Model model,
+            User profile
+    ) {
+        Optional<User> found = repository.findByLogin(login);
+        if (found.isEmpty()) {
+            return "redirect:/users/all";
+        }
+
+        profile = repository.findByLogin(login).orElseThrow();
+        model.addAttribute("user_profile", profile);
+        return "users/edit_profile";
+    }
+
+    @PostMapping("/{profile-name}/edit")
+    public String saveEditProfile(
+            @PathVariable(name = "profile-name") String login,
+            User profile
+    ) {
+        repository.save(profile);
+        return "users/edit_profile";
     }
 }
