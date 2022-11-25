22105f319e73e11da5e5df24dec386e1124e2bd3
Timkin Maxim <enderman.2016@mail.ru>
2022-11-25 10:07:49 +0300

    search user by login

--- Models/src/main/java/com/timkin/models/controller/UserController.java
+++ Models/src/main/java/com/timkin/models/controller/UserController.java
@@ -2,12 +2,10 @@ package com.timkin.models.controller;
 
 import com.timkin.models.entity.User;
 import com.timkin.models.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,6 +28,16 @@ public class UserController {
     @GetMapping("/all")
     public String openAllUsersTable(Model model) {
         List<User> all = repository.findAll();
+        model.addAttribute("users", all);
+        return "users/all_users";
+    }
+
+    @GetMapping("/search")
+    public String searchUsers(
+            @RequestParam(name = "s") String searchString,
+            Model model
+    ) {
+        List<User> all = repository.findByLoginContainsIgnoreCase(searchString);
         model.addAttribute("users", all);
         return "users/all_users";
     }
--- Models/src/main/java/com/timkin/models/repo/UserRepository.java
+++ Models/src/main/java/com/timkin/models/repo/UserRepository.java
@@ -3,9 +3,11 @@ package com.timkin.models.repo;
 import com.timkin.models.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByLogin(String login);
+    List<User> findByLoginContainsIgnoreCase(String login);
 }
