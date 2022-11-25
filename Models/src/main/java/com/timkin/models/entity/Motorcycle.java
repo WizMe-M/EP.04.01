55939c384cea16b8cc90fedf4dbecc6d30885469
WizMe-M <timkin.moxim@mail.ru>
2022-11-25 23:03:02 +0300

    motorcycle validation annotations

--- Models/src/main/java/com/timkin/models/entity/Motorcycle.java
+++ Models/src/main/java/com/timkin/models/entity/Motorcycle.java
@@ -1,6 +1,7 @@
 package com.timkin.models.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
 @Table(name = "motorcycles")
@@ -12,18 +13,31 @@ public class Motorcycle {
     private int id;
 
     @Column(length = 20, nullable = false)
+    @Size(min = 4, max = 20, message = "Model name length should be in range from 4 to 20")
+    @NotBlank(message = "Model name should not be null, not be empty and not consist of only space characters")
     private String model;
 
     @Column(nullable = false)
+    @Positive(message = "Price should be positive")
+    @Min(value = 10, message = "Minimum price is 10")
+    @Max(value = 1000000000, message = "Maximum price is 1'000'000'000")
+    @NotNull(message = "Price can't be null")
     private double price;
 
     @Column(nullable = false)
+    @NotNull(message = "Flag 'Is sold' can't be null")
     private boolean sold;
 
     @Column(name = "engine_volume", nullable = false)
+    @Positive(message = "Engine volume should be positive")
+    @Min(value = 5, message = "Minimum volume is 5")
+    @Max(value = 1000, message = "Maximum volume is 1000")
+    @NotNull(message = "Engine volume can't be null")
     private double engineVolume;
 
     @Column(name = "engine_type", length = 20, nullable = false)
+    @Size(min = 3, max = 20, message = "Model name length should be in range from 4 to 20")
+    @NotBlank(message = "Model name should not be null, not be empty and not consist of only space characters")
     private String engineType;
 
     public Motorcycle() {
