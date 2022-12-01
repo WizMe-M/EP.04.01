b80e84ce8d93a13e3fbd8df4a53b0e0adbe57cfc
WizMe-M <timkin.moxim@mail.ru>
2022-12-01 14:24:39 +0300

    finished some todos

--- Models/src/main/java/com/timkin/models/ModelsApplication.java
+++ Models/src/main/java/com/timkin/models/ModelsApplication.java
@@ -9,10 +9,4 @@ public class ModelsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ModelsApplication.class, args);
     }
-
-    // TODO: add EngineController
-    // TODO: add EngineTypeController
-
-    // TODO: add 'create/total/detail/edit views' for Engine
-    // TODO: add 'create/total/edit views' for EngineType
 }
