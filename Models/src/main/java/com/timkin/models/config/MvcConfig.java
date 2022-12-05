af57011770f6ab40ba32bd29b78e1c4ba51df03b
WizMe-M <timkin.moxim@mail.ru>
2022-12-06 00:06:08 +0300

    use styled login page

--- Models/src/main/java/com/timkin/models/config/MvcConfig.java
+++ Models/src/main/java/com/timkin/models/config/MvcConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("auth/login");
+        registry.addViewController("/login").setViewName("auth/login1");
     }
 }
