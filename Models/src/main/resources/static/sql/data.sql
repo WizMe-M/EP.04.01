614a37d35c3d0fbc577f0d03b3774f0f736955c5
WizMe-M <timkin.moxim@mail.ru>
2023-02-03 14:13:54 +0300

    do not insert if it already exists

--- Models/src/main/resources/static/sql/data.sql
+++ Models/src/main/resources/static/sql/data.sql
@@ -1,7 +1,9 @@
 insert into users(id, login, password, registration_date)
 values ('47a072c8-a19a-4ff1-96d8-b06bffecc096', 'admin',
         '$2a$16$uM0l2gWoBNbsLL431vweUefGIp685woBaRbtzM3BMMZWvBQO2WEi6',
-        now());
+        now())
+ON CONFLICT DO NOTHING;
 
 insert into authorities(user_id, role)
-values ('47a072c8-a19a-4ff1-96d8-b06bffecc096', 'Administrator');
+values ('47a072c8-a19a-4ff1-96d8-b06bffecc096', 'Administrator')
+ON CONFLICT DO NOTHING;
